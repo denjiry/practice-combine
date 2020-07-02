7fa21b8ea9398c6ea61a0576b294ecd45dbac571
kento tajiri <k.denjiry@gmail.com>
2020-07-02 22:31:52 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use combine::char::{letter, string};
+use combine::{
+    attempt, between, chainl1, many1, parser, satisfy, ParseError, ParseResult, Parser, Stream,
+    StreamOnce,
+};
+
+fn main() {
+    let mut input = "r###hello#world###";
+    println!("{:?}", rawstr(&mut input));
+}
+
+pub fn rawstr<'a, I>(input: &mut I) -> ParseResult<(), I>
+where
+    I: Stream<Item = char>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    let r = string("r").map(|_| ());
+    let sharps1 = many1::<Vec<_>, _>(string("#"));
+    let sharps2 = many1::<Vec<_>, _>(string("#"));
+    let contents = many1::<Vec<_>, _>(letter());
+    let sharps_paren = between(sharps1, sharps2, contents);
+    let mut rawstr = r.and(sharps_paren).map(|_| ());
+    rawstr.parse_stream(input)
+}
